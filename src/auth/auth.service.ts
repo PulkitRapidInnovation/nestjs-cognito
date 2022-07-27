215421158f41294a54e39910579a9c2b1b4f93a2
Pulkit <pg@rapidinnovation.dev>
2022-07-27 17:11:39 +0530

    nestjs authentication using cognito for saml

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,99 @@
+import { AuthConfig } from './auth.config';
+import { forwardRef, Inject, Injectable } from '@nestjs/common';
+import {
+  AuthenticationDetails,
+  CognitoUser,
+  CognitoUserPool,
+  CognitoUserAttribute,
+} from 'amazon-cognito-identity-js';
+
+@Injectable()
+export class AuthService {
+  private userPool: CognitoUserPool;
+  private sessionUserAttributes: {};
+  constructor(
+    @Inject(forwardRef(() => AuthConfig))
+    private readonly authConfig: AuthConfig,
+  ) {
+    this.userPool = new CognitoUserPool({
+      UserPoolId: this.authConfig.userPoolId,
+      ClientId: this.authConfig.clientId,
+    });
+  }
+
+  registerUser(registerRequest: {
+    name: string;
+    email: string;
+    password: string;
+  }) {
+    const { name, email, password } = registerRequest;
+    console.log({ name, password, email });
+    return new Promise((resolve, reject) => {
+      return this.userPool.signUp(
+        name,
+        password,
+        [new CognitoUserAttribute({ Name: 'email', Value: email })],
+        null,
+        (err, result) => {
+          console.log(
+            '🚀 ~ file: auth.service.ts ~ line 38 ~ AuthService ~ returnnewPromise ~ err',
+            err,
+          );
+          if (!result) {
+            reject(err);
+          } else {
+            resolve(result.user);
+          }
+        },
+      );
+    });
+  }
+
+  authenticateUser(user: { name: string; password: string }) {
+    const { name, password } = user;
+
+    const authenticationDetails = new AuthenticationDetails({
+      Username: name,
+      Password: password,
+    });
+    const userData = {
+      Username: name,
+      Pool: this.userPool,
+    };
+
+    const newUser = new CognitoUser(userData);
+
+    return new Promise((resolve, reject) => {
+      return newUser.authenticateUser(authenticationDetails, {
+        onSuccess: (result) => {
+          resolve(result);
+        },
+        onFailure: (err) => {
+          reject(err);
+        },
+      });
+    });
+  }
+
+  verifyUser(verifyUserDto: { name: string; code: string }) {
+    const { name, code } = verifyUserDto;
+    const userData = {
+      Username: name,
+      Pool: this.userPool,
+    };
+
+    const newUser = new CognitoUser(userData);
+    return new Promise((resolve) => {
+      return newUser.confirmRegistration(code, true, (err, result) => {
+        if (!result) {
+          resolve({
+            statusCode: 400,
+            response: err.message || JSON.stringify(err),
+          });
+        } else {
+          resolve({ statusCode: 200, message: result });
+        }
+      });
+    });
+  }
+}
